f7e8e4ea5220d9d78a7f797a89599fea5790b801
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-02-19 15:55:22 -0300

    Initial Commit

---
+++ S-SocialMedia/S_SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  S_SocialMediaApp.swift
+//  S-SocialMedia
+//
+//  Created by Isaque da Silva on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct S_SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
