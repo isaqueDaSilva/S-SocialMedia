e093a41ab870f6bfeb8a27f9a6a1763db77a807a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-02-24 16:41:30 -0300

    Updating UserProfileRow In this update I've switch the requirement to pass an entire UserProfile object to only pass the needed types like the profile picture url(Optional URL), username and a text(both string types).

--- S-SocialMedia/Resources/UI/UserProfileRow.swift
+++ S-SocialMedia/Resources/UI/UserProfileRow.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 struct UserProfileRow: View {
-    let user: UserProfile
+    let profilePictureURL: URL?
+    let username: String
+    let text: String
     
     var body: some View {
-        HStack {
-            UserProfilePicture(url: user.pictureURL)
+        
+        HStack(alignment: .top) {
+            UserProfilePicture(url: profilePictureURL)
             
             VStack(alignment: .leading) {
-                Text(user.username)
-                    .font(.subheadline)
+                Text(username)
+                    .font(.headline)
                     .fontWeight(.semibold)
                 
-                Text(user.bio ?? "")
+                Text(text)
                     .font(.subheadline)
                     .foregroundStyle(.gray)
                     .lineLimit(2)
                     .padding(.trailing, 10)
             }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    init(profilePictureURL: URL? = nil, username: String, text: String) {
+        self.profilePictureURL = profilePictureURL
+        self.username = username
+        self.text = text
     }
 }
 
 #Preview {
-    UserProfileRow(user: .mock)
+    UserProfileRow(profilePictureURL: nil, username: "something", text: "text")
 }
