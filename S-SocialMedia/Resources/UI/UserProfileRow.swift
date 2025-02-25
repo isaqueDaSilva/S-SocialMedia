3c1c9063b6638d618993a11b0360e7a49ceb894f
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-02-25 20:31:28 -0300

    Creating a row for display a user.

--- S-SocialMedia/Resources/UI/UserProfileRow.swift
+++ S-SocialMedia/Resources/UI/UserProfileRow.swift
@@ -8,15 +8,12 @@
 import SwiftUI
 
 struct UserProfileRow: View {
-    let profilePictureURL: URL?
     let username: String
     let text: String
     
     var body: some View {
         
         HStack(alignment: .top) {
-            UserProfilePicture(url: profilePictureURL)
-            
             VStack(alignment: .leading) {
                 Text(username)
                     .font(.headline)
@@ -32,13 +29,12 @@ struct UserProfileRow: View {
         .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    init(profilePictureURL: URL? = nil, username: String, text: String) {
-        self.profilePictureURL = profilePictureURL
+    init(username: String, text: String) {
         self.username = username
         self.text = text
     }
 }
 
 #Preview {
-    UserProfileRow(profilePictureURL: nil, username: "something", text: "text")
+    UserProfileRow(username: "something", text: "text")
 }
