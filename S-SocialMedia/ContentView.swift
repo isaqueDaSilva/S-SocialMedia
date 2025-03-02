70bddcc64724d84783cb57119c86597fa5a5e6c6
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-03-02 20:56:24 -0300

    Initial Commit

---
+++ S-SocialMedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  S-SocialMedia
+//
+//  Created by Isaque da Silva on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ S-SocialMedia/S_SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  S_SocialMediaApp.swift
+//  S-SocialMedia
+//
+//  Created by Isaque da Silva on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct S_SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
